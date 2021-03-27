d1edf1b18387d2878e5aafdd39b13ec9514cfddc
Ohmsiwanart <78574266+Ohmsiwanart@users.noreply.github.com>
2021-03-28 03:23:27 +0700

    Add files via upload

---
+++ kor2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sobcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            Console.Write("Input number a :");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input number b :");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;       
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            } while (x != y);
+             Console.Write(x);
+            
+        }
+    }
+}
